182656a32b9a8fab5af5dccfe6ecf5a19e6a7649
Overlord <tanvir1005@student.sust.edu>
2021-12-11 02:34:59 +0600

    4rd commit

--- app/src/main/java/com/ahmed/listfragment/MainActivity.java
+++ app/src/main/java/com/ahmed/listfragment/MainActivity.java
@@ -1,14 +1,54 @@
 package com.ahmed.listfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ahmed.listfragment.data.Course;
+
+public class MainActivity extends AppCompatActivity
+        implements CourseListFragment.Callbacks{
+
+    private boolean isTwoPane = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.detailContainer) != null) {
+            isTwoPane = true;
+        }
+
     }
+
+    @Override
+    public void onItemSelected(Course course, int position) {
+        if (isTwoPane) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("course_id", position);
+
+
+            FragmentManager fm = getSupportFragmentManager();
+            CourseDetailFragment courseDetailFragment = new CourseDetailFragment();
+            courseDetailFragment.setArguments(bundle);
+
+
+            fm.beginTransaction()
+                    .replace(R.id.detailContainer, courseDetailFragment)
+                    .commit();
+
+        }else {
+
+            Intent intent = new Intent(MainActivity.this, CourseDetailActivity.class);
+            intent.putExtra("course_id", position);
+            startActivity(intent);
+
+        }
+
+    }
+
+
 }
